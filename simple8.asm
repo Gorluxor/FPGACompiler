854e51205d0fd76ba0c387587561255a06b5ba5c
Aleksandar Cvejic <cvejicaca@gmail.com>
2019-09-20 18:11:40 +0200

    simple8.asm

--- simple8.asm
+++ simple8.asm
@@ -1,25 +1,25 @@
-	Begin_INST:
+Begin_INST:
 		MOV sp, 0xeeee
 		PUSH	 0x0
 		CALL	 main
 		HALT
 main:
-			PUSH	r7
-			MOV 	r7,sp
-			ADD		sp, 12
+		PUSH	r7
+		MOV 	r7,sp
+		ADD	sp, 12
 .main_body:
-			MOV 		r0,4
-			ST 		[r7 + 0],r0		;ASSIGN
-			MOV	r0,r7	;POINTER
-			ST 		[r7 + 4],r0		;ASSIGN
-			LD 		r0,[r7 + 4]
-			LD	 r0,[r0]
-			MOV 		r1,r0	;EXPRETION
-			MUL		r1,3
-			ST 		[r7 + 2],r1		;ASSIGN
-			LD 		r6,[r7 + 2]
-			J 	.main_exit
+		MOV	r0,4
+		ST 	[r7 + 0],r0 ;ASSIGN
+		MOV	r0,r7	    ;POINTER
+		ST 	[r7 + 4],r0 ;ASSIGN
+		LD 	r0,[r7 + 4]
+		LD	r0,[r0]
+		MOV	r1,r0       ;EXPRESSION
+		MUL	r1,3
+		ST 	[r7 + 2],r1 ;ASSIGN
+		LD 	r6,[r7 + 2]
+		J 	.main_exit
 .main_exit:
-			MOV 	sp,r7
-			POP 	r7
-			RET
+		MOV 	sp,r7
+		POP 	r7
+		RET
